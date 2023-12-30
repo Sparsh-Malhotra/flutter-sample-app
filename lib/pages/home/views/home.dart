4564cea38b9434d68ea26fdad8fe17ff8c1caecf
Sparsh Malhotra <1900300100220@ipec.org.in>
2023-12-30 13:49:03 +0530

    Removed video library route

--- lib/pages/home/views/home.dart
+++ lib/pages/home/views/home.dart
@@ -173,9 +173,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               menuItem(
                                 'video_library.png',
                                 'Video Library',
-                                () {
-                                  Get.toNamed('/video-library');
-                                },
+                                () {},
                               ),
                               menuItem('notice_board_icon.png', 'Notice Board',
                                   () => {}),
