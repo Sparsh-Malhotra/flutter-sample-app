687c699445109301dadd9806799795c99feb4d67
New Harshit Jain <harshit120299@gmail.com>
2024-03-09 20:56:47 +0530

    read resouce and video lib response changes

--- lib/pages/read_text/views/read_text.dart
+++ lib/pages/read_text/views/read_text.dart
@@ -32,13 +32,14 @@ class _ReadTextScreenState extends State<ReadTextScreen> {
             Expanded(
               child: ListView(
                 children: [
-                  Text(
-                    widget.text,
-                    style: TextStyle(
-                      fontSize: textSize,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      height: 1.5,
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontSize: textSize,
+                        color: Colors.black,
+                        height: 1.5,
+                      ),
+                      children: _highlightWords(widget.text),
                     ),
                     textAlign: TextAlign.center,
                   ),
@@ -74,5 +75,84 @@ class _ReadTextScreenState extends State<ReadTextScreen> {
         ),
       ),
     );
+  }
+
+  List<TextSpan> _highlightWords(String text) {
+    List<TextSpan> spans = [];
+
+    // Define the pattern for detecting <color>...</color>
+    RegExp pattern = RegExp(r'<(\w+)>([^<]+)<\/\1>');
+
+    // Find matches in the text
+    Iterable<Match> matches = pattern.allMatches(text);
+
+    int lastEnd = 0;
+
+    for (Match match in matches) {
+      // Add non-matching text
+      spans.add(TextSpan(text: text.substring(lastEnd, match.start)));
+
+      // Extract color and word from the match
+      String color = match.group(1)!;
+      String word = match.group(2)!;
+
+      // Add matching text with specified background color
+      if (color == 'desc') {
+        spans.add(
+          TextSpan(
+            text: word,
+            style: TextStyle(
+              fontStyle:
+                  _is_italic(color) ? FontStyle.italic : FontStyle.normal,
+              fontSize: textSize - .25*textSize,
+            ),
+            )
+          );
+      } else {
+        spans.add(TextSpan(
+            text: word,
+            style: TextStyle(
+              backgroundColor: _getColor(color),
+              fontWeight: _is_bold(color) ? FontWeight.bold : FontWeight.normal,
+              fontStyle:
+                  _is_italic(color) ? FontStyle.italic : FontStyle.normal,
+            )));
+      }
+
+      lastEnd = match.end;
+    }
+
+    // Add remaining non-matching text
+    spans.add(TextSpan(text: text.substring(lastEnd)));
+
+    return spans;
+  }
+
+  Color _getColor(String colorName) {
+    // Map color names to Color objects
+    Map<String, Color> colorMap = {
+      'r': Colors.red,
+      'b': Color.fromARGB(255, 163, 213, 251),
+      'bg': Colors.yellowAccent,
+      'y': Colors.yellow,
+      'g': Color.fromARGB(255, 192, 191, 191),
+      'lgr': Color.fromARGB(255, 156, 246, 198),
+      // Add more color mappings as needed
+    };
+
+    // Return the corresponding color or white if not found
+    return colorMap[colorName.toLowerCase()] ?? Colors.white;
+  }
+
+  bool _is_bold(String word) {
+    // Check if the word starts and ends with '**' for bold formatting
+    // return word.startsWith('**') && word.endsWith('**');
+    return word == 'bd';
+  }
+
+  bool _is_italic(String word) {
+    // Check if the word starts and ends with '*' for italic formatting
+    // return word.startsWith('*') && word.endsWith('*');
+    return word == 'i';
   }
 }
